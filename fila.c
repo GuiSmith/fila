188f20a7ab5f4c98eb8d387e5ac4566fc7632a73
Guilherme Smith Dansiguer Rodrigues <424431@unoesc.lan>
2024-10-03 21:41:41 -0300

    Structure created

--- fila.c
+++ fila.c
@@ -1,7 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TAMANHO 50
+
+class Fila {
+	public:
+		Fila();
+		bool Fila_cheia();
+		bool Fila_Vazia();
+		bool Inserir(int valor);
+		int Retirar();
+		void Ver();
+	private:
+		int inicio = -1;
+		int fim = -1;
+		int fila[TAMANHO];
+};
+
 int main(){
-	printf("Hello world!");
+	
 	return 0;
+}
+
+bool Fila::Fila_cheia(){
+	if (Fila_Vazia()){
+		return false;
+	}else{
+		/*
+		Testando se inicio é a primeira posição da fila E fim é a última posição da fila
+		OU
+		Testando se inicio é não é a primeira posição da fila e fim é a posição anterior a do inicio
+		*/
+		if((inicio == 0 && fim == TAMANHO-1) || (inicio != 0 && fim==inicio-1)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
+
+bool Fila::Fila_Vazia(){
+	if(inicio == -1 && fim == -1){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+bool Fila::Inserir(int valor){
+	if(!Fila_cheia()){
+		if(Fila_Vazia()){
+			inicio++;
+			fim++;
+			fila[fim] = valor;
+		}else{
+			fila[fim] = valor;
+		}
+		if(fim == TAMANHO-1){
+			fim = 0;
+		}else{
+			fim++;
+		}
+		return true;
+	}else{
+		return false;
+	}
 }
