587c483a93ca71ce6dd6b810a37d6524ed327ede
GuiSmith <guilhermessmith2014@gmail.com>
2024-10-16 22:18:56 -0300

    game setup finished, end game to be configured

--- uno.c
+++ uno.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #define TAMANHO 40
+#define QUANTIDADE_JOGADORES 2
 
 #ifdef _WIN32
     #define CLEAR_COMMAND "cls"
@@ -54,6 +55,8 @@ class Fila {
                 bool Fila_Vazia(bool show = false);
                 bool Inserir(Carta valor);
                 Carta Retirar();
+                bool Recalcular_Quantidade();
+                int Quantidade();
                 void Ver(bool show_qtd = false);
                 void Dados(); //Fiz isso apenas para ver os dados do vetor, incluindo dados, inicio e fim
                 void Baralho();
@@ -61,16 +64,91 @@ class Fila {
         private:
                 int inicio = -1;
                 int fim = -1;
+                int qtd = 0;
                 Carta fila[TAMANHO];
 };
 
 int main(){
     Fila baralho;
+    system(CLEAR_COMMAND);
+    printf("Criando baralho...\n");
     baralho.Baralho();
-    baralho.Ver();
+    printf("Embaralhando...\n");
     baralho.Embaralhar();
-    printf("\n\nEmbaralhando\n\n");
-    baralho.Ver();
+
+    //Criando jogadores
+    Fila jogadores[QUANTIDADE_JOGADORES];
+    for (int i = 0; i < QUANTIDADE_JOGADORES; i++){
+        jogadores[i] = Fila();
+    }
+
+    //Jogadores pegando cartas
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < QUANTIDADE_JOGADORES; j++){
+            jogadores[j].Inserir(baralho.Retirar());
+        }
+    }
+
+    /*
+    //Mostrando cartas dos jogadores
+    for (int i = 0; i < QUANTIDADE_JOGADORES; i++){
+        printf("\n\n%d° Jogador",i+1);
+        jogadores[i].Ver();
+    }
+    */
+        
+    bool game_over = false;
+    int vencedor_index;
+    int vez = 0;
+    int menor_baralho_index;
+    Fila* jogador;
+    Carta carta_descartada;
+    printf("\n\nRetirando cartas!");
+    printf("Jogador 0 inicia a jogada!");
+    carta_descartada = jogador[vez].Retirar();
+    menor_baralho_index = vez;
+    vez++;
+    while(!game_over){
+        printf("Última carta descartada: %d %s", carta_descartada.numero, carta_descartada.cor);
+
+        jogador = &jogadores[vez];
+        Carta carta_na_mao = jogador->Retirar();
+
+        printf("\nCarta na mão do %d° jogador: %d, numero %s",vez+1,carta_na_mao.numero,carta_na_mao.cor);
+
+        //Compara carta descartada com carta na mão
+        if (strcmp(carta_descartada.cor,carta_na_mao.cor) || carta_descartada.numero == carta_na_mao.numero){
+            //Descartou carta
+            carta_descartada = carta_na_mao;
+        }else{
+            //Não descartou carta
+            jogador->Inserir(carta_na_mao);
+            //Compra carta
+            carta_na_mao = baralho.Retirar();
+            
+            //Compara carta na mão com carta comprada
+            if(strcmp(carta_descartada.cor,carta_na_mao.cor) || carta_descartada.numero == carta_na_mao.numero){
+                //Descartou carta comprada
+                carta_descartada = carta_na_mao;
+            }else{
+                //Não descartou carta e inseriu no próprio baralho
+                jogador->Inserir(carta_na_mao);
+            }
+        }
+
+        //Definindo jogador com menor baralho
+        if (jogador->Quantidade() <= jogadores[menor_baralho_index].Quantidade()){
+            menor_baralho_index = vez;
+        }
+
+        //Mudando vez
+        if (vez == QUANTIDADE_JOGADORES-1){
+            vez = 0;
+        }else{
+            vez++;
+        }
+    }
+    printf("\nVencedor: %d° jogador",vencedor_index);
     printf("\n");
 }
 
@@ -78,6 +156,7 @@ Fila::Fila(bool show){
     if(show){
         printf("Fila criada\n");
     }
+    
 }
 
 bool Fila::Fila_cheia(bool show){
@@ -111,17 +190,17 @@ bool Fila::Inserir(Carta valor){
             if(Fila_Vazia()){
                 inicio++;
                 fim++;
-                fila[fim] = valor;
-             }else{
-                 if(fim == TAMANHO - 1){
-                     fim = 0;
-                 }else{
-                     fim++;
-                 }
-                 fila[fim] = valor;
-             }
-             //printf("%d: %d\n",fim,valor);
-             return true;
+            }else{
+                if(fim == TAMANHO - 1){
+                    fim = 0;
+                }else{
+                    fim++;
+                }
+            }
+            //printf("%d: %d\n",fim,valor);
+            fila[fim] = valor;
+            qtd++;
+            return true;
         }else{
             return false;
         }
@@ -144,28 +223,40 @@ Carta Fila::Retirar(){
                 inicio++;
             }
         }
+        qtd--;
         return temp;
     }
+}
+
+bool Fila::Recalcular_Quantidade(){
+    if(Fila_Vazia()){
+        return false;
+    }else{
+        //Calculando a quantidade de itens
+        if(fim > inicio){
+            qtd = (fim - inicio) + 1;
+        }else{
+            if(fim < inicio){
+                qtd = (fim + 1) + (TAMANHO - inicio);
+            }else{
+                qtd = 1;
+            }
+        }
+        return true;
+    }
+}
+
+int Fila::Quantidade(){
+    return qtd;
 }
 
 void Fila::Ver(bool show_qtd){
     if (Fila_Vazia(true)){
         exit(1);
     }
-    int qtd;
     Carta temp;
-    //Calculando a quantidade de itens
-    if(fim > inicio){
-        qtd = (fim - inicio) + 1;
-    }else{
-        if(fim < inicio){
-            qtd = (fim + 1) + (TAMANHO - inicio);
-        }else{
-            qtd = 1;
-        }
-    }
     if(show_qtd){
-        printf("\n(%s %d)",temp.cor, temp.numero);
+        printf("\n%d cartas",qtd);
     }
     //Mostrando na tela
     if(qtd == 1){
@@ -176,7 +267,7 @@ void Fila::Ver(bool show_qtd){
         for(int i = 0; i < qtd; i++){
             temp = Retirar();
             Inserir(temp);
-            printf("\n(%s %d)",temp.cor, temp.numero);
+            printf("\n%d° carta: %s %d",i+1,temp.cor, temp.numero);
         }
     }
 }
